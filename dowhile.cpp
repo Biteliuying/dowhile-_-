aed77b08104bb0775f1cf0adc71d0be4e94facd6
BITEYING <96273196+Biteliuying@users.noreply.github.com>
2022-02-09 23:23:01 +0800

    dowhile
    loop icount>=0

---
+++ dowhile.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int icount;
+	icount = 5;
+	do {
+		printf("打印出来数\
+字icount=%d\n",icount);
+		icount -= 1;
+	} 
+	while (icount >= 0);
+	printf("jiefang");
+}
